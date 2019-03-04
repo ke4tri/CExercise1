3e377acba8258c8f657c048b64dedb11c921a9f0
Wayne Collier <ke4tri@gmail.com>
2019-03-04 09:02:19 -0600

    consoling sentence with instances

--- CExercise1/Program.cs
+++ CExercise1/Program.cs
@@ -6,7 +6,46 @@ namespace CExercise1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Fishing fishLocation = new Fishing();
+            fishLocation.Type = "Trout";
+            fishLocation.Location = "Cherokee";
+
+            Books newBook = new Books();
+            newBook.Type = "Harry Potter";
+
+            //Console.WriteLine("{0} {1} {2}",
+            //    fishLocation.Type,
+            //    fishLocation.Location,
+            //    newBook.Type);
+            Console.WriteLine("I fish for {0} inside of {1} while " +
+                "readking {2}",fishLocation.Type, fishLocation.Location, newBook.Type );
+            Console.ReadLine();
+            
         }
+    }
+
+    class Fishing
+    {
+        public string Type { get; set; }
+        public string Location { get; set; }
+    }
+
+    class Books
+    {
+        public string Type { get; set; }
+        public string Cover { get; set; }
+    }
+
+    class Tacos
+    {
+        public string Type { get; set; }
+        public string Meat { get; set; }
+    }
+
+    class Shoes
+    {
+        public string Type { get; set; }
+        public string Brand { get; set; }
     }
 }
