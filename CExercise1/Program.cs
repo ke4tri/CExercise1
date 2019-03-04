db8a143213c53276f83f5c7aaa6ecf37d428dda9
Wayne Collier <ke4tri@gmail.com>
2019-03-04 16:53:48 -0600

    working

--- CExercise1/Program.cs
+++ CExercise1/Program.cs
@@ -4,48 +4,51 @@ namespace CExercise1
 {
     class Program
     {
+
+        class Fishing
+        {
+            public string Type { get; set; }
+            public string Location { get; set; }
+        }
+
+        class Books
+        {
+            public string Type { get; set; }
+            public string Cover { get; set; }
+        }
+
+        class Tacos
+        {
+            public string Type { get; set; }
+            public string Meat { get; set; }
+        }
+
+        class Shoes
+        {
+            public string Type { get; set; }
+            public string Brand { get; set; }
+        }
+
+
         static void Main(string[] args)
         {
 
-            Fishing fishLocation = new Fishing();
-            fishLocation.Type = "Trout";
-            fishLocation.Location = "Cherokee";
+            Fishing fishLocation = new Fishing
+            {
+                Type = "Trout",
+                Location = "Cherokee"
+            };
 
             Books newBook = new Books();
             newBook.Type = "Harry Potter";
 
-            //Console.WriteLine("{0} {1} {2}",
-            //    fishLocation.Type,
-            //    fishLocation.Location,
-            //    newBook.Type);
+
             Console.WriteLine("I fish for {0} inside of {1} while " +
-                "readking {2}",fishLocation.Type, fishLocation.Location, newBook.Type );
+                "reading {2}",fishLocation.Type, fishLocation.Location, newBook.Type );
             Console.ReadLine();
             
         }
     }
 
-    class Fishing
-    {
-        public string Type { get; set; }
-        public string Location { get; set; }
-    }
-
-    class Books
-    {
-        public string Type { get; set; }
-        public string Cover { get; set; }
-    }
-
-    class Tacos
-    {
-        public string Type { get; set; }
-        public string Meat { get; set; }
-    }
-
-    class Shoes
-    {
-        public string Type { get; set; }
-        public string Brand { get; set; }
-    }
+   
 }
